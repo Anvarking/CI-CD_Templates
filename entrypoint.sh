a48d12aa83817190037801881b517392dcd87472
Anvar <anvar.shermatov.99@mail.ru>
2025-05-18 15:57:05 +0300

    Try to self host run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Заменить переменными среды: GITHUB_URL и RUNNER_TOKEN
-if [ -z "$https://github.com/Anvarking/CI-CD_Templates" ] || [ -z "$AR65QGMXJFNSYNOKP7ROCXTIFHS4E" ]; then
+# Проверка: переданы ли переменные окружения
+if [ -z "$GITHUB_URL" ] || [ -z "$RUNNER_TOKEN" ]; then
   echo "❌ Не указаны GITHUB_URL или RUNNER_TOKEN"
   exit 1
 fi
 
 # Настройка раннера
-./config.sh --url "$https://github.com/Anvarking/CI-CD_Templates" --token "$AR65QGMXJFNSYNOKP7ROCXTIFHS4E" --unattended --name docker-runner --labels flask,python
+./config.sh --url "$GITHUB_URL" --token "$RUNNER_TOKEN" --unattended --name docker-runner --labels flask,python
 
 # Запуск раннера
 ./run.sh
