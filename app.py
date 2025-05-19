e880e4b4c2a2e3892f977695a4dc667d4bac731b
Anvar <anvar.shermatov.99@mail.ru>
2025-05-19 08:31:42 +0300

    Added Port to view in brouser

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from GitHub Runner!"
+
+if __name__ == '__main__':
+    # Получаем порт из переменной окружения или используем 3500 по умолчанию
+    port = int(os.environ.get('PORT', 3500))
+    app.run(host='0.0.0.0', port=port, debug=True)
